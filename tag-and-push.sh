f23e86cacd8df735558c32f940bce7c0f5a85465
AJ Slater <aj@slater.net>
2020-02-26 13:19:59 -0800

    fix config for 2.1 and consolidate post push script

---
+++ tag-and-push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+source ./env
+VERSION=${ALPINE_VERSION}_${PKG_VERSION}
+TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
+docker tag $IMAGE "$IMAGE:$TAG"
+docker tag $IMAGE $IMAGE:latest
+docker push $REPO
--- tag-latest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ./env
-docker tag $IMAGE $IMAGE:latest;
-docker push $IMAGE:latest
