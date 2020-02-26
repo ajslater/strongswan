0ba33a9cf128cfd88a1ed21f8ce50ad64f2f088c
AJ Slater <aj@slater.net>
2020-02-26 13:38:38 -0800

    login just before push

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -5,4 +5,5 @@ VERSION=${ALPINE_VERSION}_${PKG_VERSION}
 TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
 docker tag $IMAGE "$IMAGE:$TAG"
 docker tag $IMAGE $IMAGE:latest
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 docker push $REPO
